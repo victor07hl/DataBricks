fa2b294298d2e7b9aedce5b7b5849914fa853483
victor07hl@gmail.com <victor07hl@gmail.com>
2022-02-02 13:59:21 +0000

    drawing in plt
    third commit from databricks

--- notebook2.py
+++ notebook2.py
@@ -1,5 +1,17 @@
 # Databricks notebook source
 import pyspark.sql.functions as F
+import matplotlib.pyplot as plt
+import numpy as np
+
+# COMMAND ----------
+
+x = np.array([0,1,2,3,4])
+y = x**2
+
+# COMMAND ----------
+
+plt.plot(x,y)
+plt.show()
 
 # COMMAND ----------
 
